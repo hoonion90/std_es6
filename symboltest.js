c3c15452c3519e09d01bb5cb511cba1d0ff63d5b
hoonion90 <clearch21@gmail.com>
2021-08-31 23:35:18 +0900

    Make Symbol

---
+++ symboltest.js
@@ -0,0 +1,18 @@
+let symbol1 = Symbol('key');
+let symbol2 = Symbol('key');
+
+document.write(typeof(symbol1));
+document.write("<br>");
+document.write("<br>");
+console.log(symbol1 = symbol2);
+console.log(symbol1 == symbol2);
+console.log(symbol1 === symbol2);
+console.log(symbol1);
+
+let obj = {};
+obj[symbol1] = 'value';
+obj[symbol2] = 'value';
+console.log(obj[symbol1] === 'value');
+console.log(obj[symbol2] === 'value');
+console.log(obj[symbol1] === obj[symbol2]);
+
