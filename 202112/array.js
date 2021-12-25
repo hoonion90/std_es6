e3cbae1ff2708b75f463efce1e223fa0e5b58df4
hoonion90 <clearch21@gmail.com>
2021-12-25 18:45:10 +0900

    메리크리스마스

--- 202112/array.js
+++ 202112/array.js
@@ -2,7 +2,7 @@ let arrTemp = ["비디오/DVD","CD","영화","콘서트","스포츠 행사","라
 
 //for
 console.log("==========for=========");
-for (i = 0; i <= arrTemp.length; i++){
+for (i = 0; i < arrTemp.length; i++){
     console.log(arrTemp[i]);
 }
 
